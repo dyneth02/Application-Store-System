683b58d4f116d292d21d6b7de56301a5c8171d61
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2025-11-30 06:09:50 +0530

    Add files via upload

---
+++ php/updateFAQ.php
@@ -0,0 +1,35 @@
+<?php session_start();
+require_once('./connection.php');
+$userid=$_GET['userId'];?>
+
+<?php 
+    if(!isset($_SESSION['userid'])) {
+        header('Location:./login_page.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ Page</title>
+    <link rel="stylesheet" href="styles/updatefeedback.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Submit your Question</h1>
+        <form id="updatefeedbackForm" action="updateFAQHandler.php?userId=<?php echo $userid?>" method="POST">
+
+            
+            <label for="feedback">Question:</label>
+            <textarea id="feedback" name="faq" rows="4" required></textarea>
+            
+            <button type="update" name="update">Update</button>
+        </form>
+        
+        <h2>Thank You</h2>
+    </div>
+    
+</body>
+</html>
