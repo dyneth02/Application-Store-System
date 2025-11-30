58c48f8af44298d54bf55b946897ada8b081035c
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2025-11-30 06:11:21 +0530

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>appPlus footer</title>
+    <!---------Link to css----------->
+    <link rel="stylesheet" href="../styles/footer.css">
+    <!----------box icons----------->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+</head>
+<body>
+    <!----------footer------------>
+    <footer>
+        <div class="footer-content">
+            <div class="logo">
+                <a href="#"><i class='bx bxl-firebase'></i>appPlus</a>
+            </div>
+            <p>Enjoy discovering and downloading  latest apps and games, all within a sleek</p>
+            <div class="icons">
+                <a href="#"><i class='bx bxl-facebook-circle'></i></a>
+                <a href="#"><i class='bx bxl-twitter' ></i></a>
+                <a href="#"><i class='bx bxl-youtube' ></i></a>
+                <a href="#"><i class='bx bxl-linkedin-square'></i></a>
+                <a href="#"><i class='bx bxl-instagram-alt'></i></a>
+            </div>
+            <p>&copy;2024 appPlus, All rights recieved</p>
+        </div>
+        <div class="footer-content">
+            <h4>Quick Links</h4>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Apps</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+        <div class="footer-content">
+            <h4>Get help</h4>
+            <ul>
+                <li><a href="contact us.php">Contact Us</a></li>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Feedbacks</a></li>
+            </ul>
+        </div>
+        <div class="footer-content">
+            <h4>Legal</h4>
+            <ul>
+                <li><a href="#">Terms of Use</a></li>
+                <li><a href="#">Privacy policy</a></li>
+            </ul>
+        </div>
+        <div class="footer-content">
+            <form>
+                <h4>Stay Connected!</h4>
+                <p>Sign up for our newsletter to get the latest app updates, exclusive offers, and tech tips delivered straight to your inbox.</p>
+                <i class='bx bx-envelope' ></i>
+                <input type="email" placeholder="Enter your email" required>
+                <button type="submit"><i class='bx bx-arrow-back bx-flip-horizontal' ></i></button>
+            </form>
+        </div> 
+    </footer>
+</body>
+</html>
